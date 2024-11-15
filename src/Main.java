fc2adcf896a6781378fe5320c0fdae9c8dda1535
cerberdll <kpojlukkreativnyj@gmail.com>
2024-11-15 22:44:21 +0100

    HW05.11.2024 is done.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Bank bank = new Bank("ПримерБанк");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Создаем банкомат
+        ATM atm1 = new ATM(50, 30);
+        atm1.initializeATM(Map.of(
+                500, 10,
+                200, 20,
+                100, 30,
+                50, 50
+        ));
+
+        // Создаем второй банкомат
+        ATM atm2 = new ATM(20, 50);
+        atm2.initializeATM(Map.of(
+                100, 10,
+                50, 20,
+                20, 30
+        ));
+
+        bank.addATM(atm1);
+        bank.addATM(atm2);
+
+        // Вывод информации о банке и банкоматах
+        bank.displayBankInfo();
+
+        // Снятие денег
+        try {
+            Map<Integer, Integer> withdrawal = atm1.withdrawMoney(870);
+            System.out.println("Выдано:");
+            withdrawal.forEach((denomination, count) ->
+                    System.out.println(denomination + " грн: " + count + " банкнот"));
+        } catch (ATMException e) {
+            System.out.println("Ошибка: " + e.getMessage());
         }
+
+        // Вывод обновленной информации
+        bank.displayBankInfo();
     }
 }
