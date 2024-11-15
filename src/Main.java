c392249c9026427d91e1a1a9b8b60b1af613587d
cerberdll <kpojlukkreativnyj@gmail.com>
2024-11-15 21:13:41 +0100

    HW26.10.2024 is done.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        LibraryCatalog catalog = new LibraryCatalog();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Добавляем книги
+        catalog.addItem(new Book("Война и мир", "Лев Толстой", "Роман", 1225));
+        catalog.addItem(new Book("1984", "Джордж Оруэлл", "Антиутопия", 328));
+
+        // Добавляем газеты
+        catalog.addItem(new Newspaper("Известия", "01.11.2024", List.of("Экономика растет", "Погода ухудшается")));
+
+        // Добавляем альманах
+        catalog.addItem(new Almanac("Литературный альманах", List.of(
+                new Book("Гарри Поттер", "Джоан Роулинг", "Фэнтези", 500),
+                new Book("1984", "Джордж Оруэлл", "Антиутопия", 328)
+        )));
+
+        // Выводим каталог
+        System.out.println("Каталог библиотеки:");
+        catalog.displayCatalog();
+
+        // Поиск по названию
+        System.out.println("Поиск по названию '1984':");
+        catalog.searchByTitle("1984");
+
+        // Поиск по автору
+        System.out.println("Поиск по автору 'Джордж Оруэлл':");
+        catalog.searchByAuthor("Джордж Оруэлл");
     }
 }
